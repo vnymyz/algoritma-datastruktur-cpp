e1db4617c93d2fd532abdb8c68688f3f2dd5d66b
Vanya <mayazurav@gmail.com>
2025-10-08 10:23:08 +0700

    Update DSA

--- Pert18 - Sorting I/bubble-sort.c
+++ Pert18 - Sorting I/bubble-sort.c
@@ -14,6 +14,11 @@ void bubbleSort(int arr[], int n) {
 
 int main() {
     int arr[] = {5, 3, 8, 4, 2};
+    // 5, 3, 8, 4, 2
+    // 3, 5, 8, 4, 2
+    // 3, 8, 5, 4, 2
+    // 3, 4, 5, 8, 2
+    // 2, 3, 4, 5, 8
     int n = sizeof(arr)/sizeof(arr[0]);
 
     bubbleSort(arr, n);
